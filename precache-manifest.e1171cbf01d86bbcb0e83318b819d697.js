189fc9946a72a9b1f3691126601be758c7dd8ff4
Gaspar Teixeira <gaspar.teixeira@gmail.com>
2020-01-09 19:40:17 -0500

    Updates

---
+++ precache-manifest.e1171cbf01d86bbcb0e83318b819d697.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0f780343179ecc2ff72c789b46b89c1c",
+    "url": "/rest-api/index.html"
+  },
+  {
+    "revision": "70becd925fdbdf3ba11d",
+    "url": "/rest-api/static/css/main.d1b05096.chunk.css"
+  },
+  {
+    "revision": "228b962df644e98586c2",
+    "url": "/rest-api/static/js/2.25ed1a27.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/rest-api/static/js/2.25ed1a27.chunk.js.LICENSE"
+  },
+  {
+    "revision": "70becd925fdbdf3ba11d",
+    "url": "/rest-api/static/js/main.2f7c5bc9.chunk.js"
+  },
+  {
+    "revision": "b26cb5929518da428089",
+    "url": "/rest-api/static/js/runtime-main.9e619214.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/rest-api/static/media/logo.5d5d9eef.svg"
+  }
+]);
